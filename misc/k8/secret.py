35c036b749ddb9928a91d493d1f27369dd0a72d4
Reza Rahim <reza.rahim@gmail.com>
2025-09-08 12:22:11 -0500

    Update secret.py

--- misc/k8/secret.py
+++ misc/k8/secret.py
@@ -1,27 +1,10 @@
-
 #!/usr/bin/env python3
 import sys
 import yaml  # pip install pyyaml
 
-# Fields we want to drop from metadata
-DROP_FIELDS = {
-    "creationTimestamp",
-    "resourceVersion",
-    "uid",
-    "generation",
-    "managedFields",
-    "selfLink",
-}
-
 def clean_k8s_obj(obj):
-    if "metadata" in obj:
-        for f in DROP_FIELDS:
-            obj["metadata"].pop(f, None)
-        # Remove noisy annotations if present
-        if "annotations" in obj["metadata"]:
-            obj["metadata"]["annotations"].pop("kubectl.kubernetes.io/last-applied-configuration", None)
-            if not obj["metadata"]["annotations"]:
-                obj["metadata"].pop("annotations")
+    # Drop metadata entirely if present
+    obj.pop("metadata", None)
     return obj
 
 if __name__ == "__main__":
