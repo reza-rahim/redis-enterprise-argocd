dbb0236b0e34c2fd64dc102e389421118bd81613
Reza Rahim <reza.rahim@gmail.com>
2025-11-03 14:40:10 -0600

    Implement S3 object listing with boto3
    Add functionality to list objects in an S3 bucket using boto3.

--- misc/boto.py
+++ misc/boto.py
@@ -17,3 +17,17 @@ for line in lines:
     print(line)
 
 ```
+```
+import boto3
+
+s3 = boto3.client('s3')
+bucket_name = 'your-bucket-name'
+
+# Create a paginator for list_objects_v2
+paginator = s3.get_paginator('list_objects_v2')
+
+for page in paginator.paginate(Bucket=bucket_name):
+    for obj in page.get('Contents', []):
+        print(obj['Key'])
+
+```
